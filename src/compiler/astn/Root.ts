38063f056820589e475d4da6f97367a25c9d4de0
Benyamin Galeano <benyamin.galeano@gmail.com>
2022-04-19 22:26:26 -0600

    style: 💄 remove console log

--- src/compiler/astn/Root.ts
+++ src/compiler/astn/Root.ts
@@ -1,9 +1,7 @@
 import { ASTNode } from "compiler"
 
 export class Root implements ASTNode {
-  constructor (public id: string, public children: ASTNode) {
-    console.log("id encontrado: ", id)
-  }
+  constructor (public id: string, public children: ASTNode) { }
 
   generate (): { [key: string]: unknown } {
     const json = this.children ? this.children.generate() : {}
