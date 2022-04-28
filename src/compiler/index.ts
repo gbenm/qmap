d8d5bc1440002025997c30a7c7539d0f4b5bb9c7
Benyamin Galeano <benyamin.galeano@gmail.com>
2022-04-27 21:42:15 -0600

    feat: 🎸 error list

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -7,12 +7,18 @@ export * from "./SymbolTable"
 export * from "./astn"
 
 export function compile (query: string | undefined | null) {
+  const errors: unknown[] = []
   const chars = new antlr.InputStream(query ?? "")
   const lexer = new qMapLexer(chars)
   const tokens = new antlr.CommonTokenStream(lexer)
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const parser = new qMapParser(tokens) as any
   parser.buildParseTrees = true
-  const tree = parser.start()
-  return tree.json
+  try {
+    const tree = parser.start()
+    return { ...tree.json, errors }
+  } catch (error: unknown) {
+    errors.push(error)
+  }
+  return { errors }
 }
