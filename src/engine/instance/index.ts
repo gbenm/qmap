d90af6f006b17d0b62d522d42f07727bdd9861d7
Benyamin Galeano <benyamin.galeano@gmail.com>
2022-05-18 22:54:30 -0600

    feat: 🎸 return of qmap function

--- src/engine/instance/index.ts
+++ src/engine/instance/index.ts
@@ -1,9 +1,41 @@
-import { compile } from "../../compiler"
+import { compile, RootQueryNode } from "../../compiler"
 import { Nullable } from "../../utils/types"
 import { QMapContext } from "../creator/types"
+import { QMapIndex } from "./types"
 
 export { QMap } from "./types"
 
+export function includes(this: RootQueryNode, path: string[]): boolean {
+  let key: string
+  let descriptor: QMapIndex | undefined = this.descriptor as QMapIndex
+  path = [...path]
+
+  for (key = path.shift() as string; path.length > 0; key = path.shift() as string) {
+    descriptor = descriptor?.[key]
+    if (!descriptor) {
+      return false
+    }
+  }
+
+  if (descriptor?.all) {
+    return !descriptor?.exclude[key]
+  }
+
+  return !!descriptor?.index[key]
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function apply(this: RootQueryNode, target: any): any {
+  // TODO: implementar algoritmo para aplicar definitions
+  return target
+}
+
 export function qmap(this: QMapContext, target: Nullable<string>) {
-    const { query, definitions, descriptor, errors } = compile(target)
+  const root = compile(target)
+
+  return {
+    apply: apply.bind(root),
+    includes: includes.bind(root),
+    errors: root.errors.length > 0 ? root.errors : undefined,
+  }
 }
--- src/engine/instance/types.ts
+++ src/engine/instance/types.ts
@@ -2,3 +2,13 @@ import { Nullable } from "../../utils/types"
 import { QMapContext } from "../creator/types"
 
 export type QMap = ((query: Nullable<string>) => void) & QMapContext
+
+export interface QMapIndex {
+  index: {
+    [key: string]: QMapIndex
+  },
+  all: boolean
+  exclude: {
+    [key: string]: boolean
+  }
+}
