00bdc0da9b5e07b2eebd8002893fda18ad6c0d61
Benyamin Galeano <benyamin.galeano@gmail.com>
2022-06-28 22:23:35 -0600

    Deploy website - based on 85aa617e013715241d088f9ca00ce80627634d55

---
+++ assets/js/caaf477a.b35eda6f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[723],{9155:e=>{e.exports=JSON.parse('{"title":"Queries","slug":"/category/queries","permalink":"/qmap/docs/category/queries","navigation":{"previous":{"title":"La instancia","permalink":"/qmap/docs/instance"},"next":{"title":"Select","permalink":"/qmap/docs/queries/select"}}}')}}]);
