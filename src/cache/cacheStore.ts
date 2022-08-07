292c2617e31a18f57e8fcded205f049144ba82ff
Benyamin Galeano <benyamin.galeano@gmail.com>
2022-08-07 15:56:51 -0600

    feat: 🎸 set compute function

--- src/cache/cacheStore.ts
+++ src/cache/cacheStore.ts
@@ -1,4 +1,5 @@
 import { CachePolicy } from "./cachePolicy"
+import { ComputeFn } from "./types"
 
 export abstract class CacheStore<Key> {
   private policy!: CachePolicy<string, string>
@@ -14,6 +15,14 @@ export abstract class CacheStore<Key> {
     return this
   }
 
+  public setComputeFn(fn: ComputeFn): void {
+    this.policy.setComputeFn(fn)
+  }
+
+  public computeAndSave(key: string, data: unknown): string {
+    return this.policy.computeAndSave(key, data)
+  }
+
   public save(key: string, data: unknown): string {
     return this.policy.save(key, data)
   }
@@ -26,9 +35,9 @@ export abstract class CacheStore<Key> {
     return this.policy.remove(key)
   }
 
-  abstract _save(key: Key, data: unknown): Key
-  abstract _lookup(key: Key): unknown
-  abstract _remove(key: Key): unknown
+  protected abstract _save(key: Key, data: unknown): Key
+  protected abstract _lookup(key: Key): unknown
+  protected abstract _remove(key: Key): unknown
 }
 
 class CacheStoreWrapper<Key> extends CachePolicy<Key, Key> {
@@ -37,14 +46,14 @@ class CacheStoreWrapper<Key> extends CachePolicy<Key, Key> {
   }
 
   save(key: Key, data: unknown): Key {
-    return this.store._save(key, data)
+    return this.store["_save"](key, data)
   }
 
   lookup(key: Key): unknown {
-    return this.store._lookup(key)
+    return this.store["_lookup"](key)
   }
 
   remove(key: Key): unknown {
-    return this.store._remove(key)
+    return this.store["_remove"](key)
   }
 }
